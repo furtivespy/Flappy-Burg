b1d43684917d57ab846ddf2493e132987572665b
Will <furtivespy@gmail.com>
2015-01-06 12:41:19 -0500

    Sharing fix
    fixed game name in sharing

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -82,18 +82,18 @@ var Share = me.GUI_Object.extend({
     },
 
     onClick: function(event) {
-        var shareText = 'Just made ' + game.data.steps + ' steps on Clumsy Bird! Can you beat me? Try online here!';
-        var url = 'http://ellisonleao.github.io/clumsy-bird/';
+        var shareText = 'Just made ' + game.data.steps + ' forks in Flappy Burg! Can you beat me? Try online here!';
+        var url = 'https://ginadamico.wordpress.com/2015/01/06/the-hellhole-has-opened/';
         FB.ui(
             {
              method: 'feed',
-             name: 'My Clumsy Bird Score!',
+             name: 'My Flappy Burg Score!',
              caption: "Share to your friends",
              description: (
                     shareText
              ),
              link: url,
-             picture: 'http://ellisonleao.github.io/clumsy-bird/data/img/clumsy.png'
+             picture: 'http://furtivespy.github.io/Flappy-Burg/data/img/clumsy.png'
             }
         );
         return false;
@@ -111,8 +111,8 @@ var Tweet = me.GUI_Object.extend({
     },
 
     onClick: function(event) {
-        var shareText = 'Just made ' + game.data.steps + ' steps on Clumsy Bird! Can you beat me? Try online here!';
-        var url = 'http://ellisonleao.github.io/clumsy-bird/';
+        var shareText = 'Just made ' + game.data.steps + ' forks on Flappy Burg! Can you beat me? Try online here!';
+        var url = 'https://ginadamico.wordpress.com/2015/01/06/the-hellhole-has-opened/';
         var hashtags = 'clumsybird,melonjs'
         window.open('https://twitter.com/intent/tweet?text=' + shareText + '&hashtags=' + hashtags + '&count=' + url + '&url=' + url, 'Tweet!', 'height=300,width=400')
         return false;
